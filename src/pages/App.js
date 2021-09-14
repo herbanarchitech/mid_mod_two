fe18ed54f929b63e3c99bbb427a8e4e5dd484f45
herbanarhitech <jeff.badette@protonmail.com>
2021-09-14 01:12:41 -0400

    very initial commit

---
+++ src/pages/App.js
@@ -0,0 +1,32 @@
+// imports
+import { Switch, Route } from 'react-router-dom'
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer'
+// pages
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+// css
+import '../App.css'
+
+const App = () => {
+  return (
+    <div id="app">
+    <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
+    <Footer />
+  </div>
+  );
+};
+
+export default App;
