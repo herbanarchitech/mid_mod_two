40ae5969bef1d1ec7277e0264339c2e0c230a620
herbanarhitech <jeff.badette@protonmail.com>
2021-09-14 02:30:45 -0400

    updated list of objects

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,4 @@
-// imports
+import React, {useState}from 'react'
 import { Switch, Route } from 'react-router-dom'
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer'
@@ -10,6 +10,42 @@ import Contact from './Contact'
 import '../App.css'
 
 const App = () => {
+  const [linuxDisros] = useState({
+
+    OS:
+    [{
+        
+          Distibution:Fedora,
+          Release:33,
+          codename:Werewolf,
+          mascot:false,
+          status:Active,
+        },
+
+         { Distibution:OpenSuse,
+          Release:15.3,
+          Codename:Malachite,
+          Mascot:true,
+          Status:Active
+          },
+         
+          {
+          distibution:ClearLinux,
+          release:350000,
+          codename:false,
+          mascot:Penguin,
+
+          },
+
+          {distibution:Arch_Linux,
+          release:2021.09,
+          codename:false,
+          mascot:false,
+          },
+        ]
+  })
+
+const distroList = linuxDisros.OS.map(()=>console.log('HELLO'));
   return (
     <div id="app">
     <NavBar />
